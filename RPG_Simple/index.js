0ea60ed7b536f1a35a70424e0e268a707d5da7c2
CodesByPeter <petermusumba360@gmail.com>
2025-08-08 14:37:15 +0300

    feat: declared game stats and accessed controls buttons

---
+++ RPG_Simple/index.js
@@ -0,0 +1,18 @@
+// Declaring and initialization of starting points[XP]
+let XP = 0;
+// Declaring and initialization of starting health[health]
+let health = 100;
+// Declaring and initialization of starting wealth[gold]
+let gold = 50;
+// Declaring and initialization of starting number of weapons[currentWeaponIndex]
+let currentWeaponIndex = 0;
+// Declaring of starting fights[fighting]
+let fighting;
+// Declaring of starting health of monster[monsterHealth]
+let monsterHealth;
+// Declaring and initialization of starting weapons having[inventory]
+let inventory = ["stick"];
+// Accessing the buttons under controls
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
