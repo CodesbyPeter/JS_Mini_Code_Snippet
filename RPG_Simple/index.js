a4a087687caaacd2f97ced23a51b33ddb7a5e651
CodesByPeter <petermusumba360@gmail.com>
2025-08-11 10:19:37 +0300

    feat: added buyWeapons function logic

--- RPG_Simple/index.js
+++ RPG_Simple/index.js
@@ -107,6 +107,26 @@ function buyHealth() {
 }
 // Creating buyWeapon function to handle all weaponBuying logic
 function buyWeapon() {
+  if (currentWeaponIndex < weapons.length - 1) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeaponIndex++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeaponIndex].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
+  }
+}
+// Creating sellWeapon function to handle all weaponSelling logic
+function sellWeapon() {
   
 }
 // Creation of functions to be used in the cave object
