8fe75f89c6678eda040e78546a067438775dd06c
CodesByPeter <petermusumba360@gmail.com>
2025-08-13 14:36:18 +0300

    feat: added calorieCounter, budgetNumberInput, entryDropdown, addEntryButton, clearButton, output and isError variables

---
+++ Calorie_Counter/index.js
@@ -0,0 +1,14 @@
+// CalorieCounter
+const calorieCounter = document.getElementById('calorie-counter');
+// BudgetNumberInput
+const budgetNumberInput = document.getElementById('budget');
+// EntryDropdown
+const entryDropdown = document.getElementById('entry-dropdown');
+// AddEntryButton
+const addEntryButton = document.getElementById('add-entry');
+// ClearButton
+const clearButton = document.getElementById('clear');
+// Output
+const output = document.getElementById('output');
+// isError
+let isError = false;
