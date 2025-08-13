08835c496c95d9e74751d6479930cb37ddb7d231
CodesByPeter <petermusumba360@gmail.com>
2025-08-13 15:57:48 +0300

    feat: added cleanInputString and isInvalidInput functions with str as parameters and regex use case

--- Calorie_Counter/index.js
+++ Calorie_Counter/index.js
@@ -12,3 +12,25 @@ const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 // isError
 let isError = false;
+
+// Cleaning up input in DailyCalories removing + and -
+function cleanInputString(str) {
+  // Testing workability
+  // console.log("original string: ", str);  
+  // Defining pattern
+  const regex = /[+-\s]/g; // g flag for global search
+  // Replacing pattern with empty string
+  return str.replace(regex, "");
+}
+// Testing if cleanInputString is working seamlessly
+// console.log(cleanInputString("+-99"));
+
+// Filtering up input
+function isInvalidInput(str) {
+  // Defining valid input pattern
+  const regex = /\d+e\d+/i; // i flag is for insensitivity
+  // Matching the valid input pattern
+  return str.match(regex);
+}
+// Testing if isInvalidInput is working seamlessly
+// console.log(isInvalidInput("1e3"));
